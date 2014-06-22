c7abb0ca123dac4ae267593afb0005ce9d197418
Allan Lykke Christensen <allan@getconverge.com>
2014-06-22 21:29:48 +0000

    Setting up the default database

--- manifests/classes/convergedb.pp
+++ manifests/classes/convergedb.pp
@@ -1,17 +1,16 @@
 
 class convergedb {
 
-	$databases = {
-  		'converge' => {
-    			ensure  => 'present',
-    			charset => 'utf8',
-  		},
-	}
 
-class { '::mysql::server':
-  root_password => 'd3vmysql',
-  override_options => { 'mysqld' => { 'max_connections' => '1024' } },
-  databases => $databases,
+class { '::mysql::server': }
+	
+mysql::db { 'converge':
+  user     => 'converge',
+  password => 'converge',
+  host     => 'localhost',
+  ensure   => 'present',
+  enforce_sql => 'true',
+  sql      => '/etc/puppet/files/converge.sql',
 }
 
 }
